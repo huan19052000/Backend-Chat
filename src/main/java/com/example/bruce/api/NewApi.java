87a7ae9eea99b99f030970bc515bd441abd0d6b5
huanbeo <huanbeo2k@gmail.com>
2022-05-08 16:27:49 +0700

    make it better

--- src/main/java/com/example/bruce/api/NewApi.java
+++ src/main/java/com/example/bruce/api/NewApi.java
@@ -2,16 +2,22 @@ package com.example.bruce.api;
 
 import com.example.bruce.dto.NewDTO;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class NewApi {
+    private List<NewDTO> newDTOS = new ArrayList<>();
     @RequestMapping(value = "/new", method = RequestMethod.POST)
     @ResponseBody
     public NewDTO createNew(@RequestBody NewDTO model) {
         return model;
+    }
+
+    @GetMapping("/new")
+    public Object getAllNew() {
+        return newDTOS;
     }
 }
